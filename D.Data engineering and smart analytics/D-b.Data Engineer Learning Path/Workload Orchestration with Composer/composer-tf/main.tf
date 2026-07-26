2cd7116874b0f3853d9196cb36e51213c5a67280
moonjukhim <moonju.khim@gmail.com>
2026-07-26 11:11:15 +0900

    Create main.tf

---
+++ D.Data engineering and smart analytics/D-b.Data Engineer Learning Path/Workload Orchestration with Composer/composer-tf/main.tf
@@ -0,0 +1,37 @@
+resource "google_project_service" "composer_api" {
+    project = var.project_id
+    service = "composer.googleapis.com"
+    disable_on_destroy = false
+}
+
+resource "google_service_account" "composer_account" {
+    account_id   = "composer-account"
+    display_name = "Custom SA for Cloud Composer Nodes"
+}
+
+resource "google_project_iam_member" "composer_account_bindings" {
+    for_each = toset([
+        "roles/composer.worker",
+    ])
+
+    project  = var.project_id
+    member   = "serviceAccount:${google_service_account.composer_account.email}"
+    role     = each.value
+}
+
+resource "google_composer_environment" "lab_environment" {
+    name    = "lab-environment"
+    project = var.project_id
+    region  = var.default_region
+
+        config {
+            software_config {
+                image_version = "composer-3-airflow-2"
+            }
+            node_config {
+                service_account = google_service_account.composer_account.email
+            }
+
+        environment_size = "ENVIRONMENT_SIZE_SMALL"
+    } // ENVIRONMENT_SIZE_MEDIUM, ENVIRONMENT_SIZE_LARGE
+}
